5f00ee9351f6bc641d116caa76aff73295ea032c
Donnie Adams <donald.g.adams@me.com>
2017-05-27 16:45:36 -0700

    Contact me page done

--- js/homeemail.js
+++ js/homeemail.js
@@ -1,10 +1,23 @@
 var emailLink = document.getElementById('ic-email');
 if (emailLink != null) {
   emailLink.href = 'mailto:thedadams@icloud.com';
-  emailLink.innerHTML = 'thedadams@icloud.com';
+  if (emailLink.innerHTML == "") {
+    emailLink.innerHTML = 'thedadams@icloud.com';
+  }
 }
+
 emailLink = document.getElementById('th-email');
 if (emailLink != null) {
   emailLink.href = 'mailto:donnie@thedadams.com';
-  emailLink.innerHTML = 'donnie@thedadams.com';
+  if (emailLink.innerHTML == "") {
+    emailLink.innerHTML = 'donnie@thedadams.com';
+  }
+}
+
+emailLink = document.getElementById('pt-email');
+if (emailLink != null) {
+  emailLink.href = 'mailto:thedadams@protonmail.com';
+  if (emailLink.innerHTML == "") {
+    emailLink.innerHTML = 'thedadams@protonmail.com';
+  }
 }
