a3fb463f9b5ad0897cac3ff53cf25a6baf2ca098
Donnie Adams <donnie@thedadams.com>
2024-03-23 14:10:15 -0400

    chore: font and design adjustments
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

--- src/components/NavBarItem.vue
+++ src/components/NavBarItem.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 const props = defineProps<{
 	page: string
 	current: boolean
@@ -8,9 +8,9 @@ const capPage = props.page.charAt(0).toUpperCase() + props.page.slice(1)
 </script>
 
 <template>
-	<a :href="page + '.html'"
-	   class="no-underline rounded-md hover:bg-gray-800 hover:text-white px-3 md:px-6 py-2 md:py-3 block sm:inline text-center"
-	   :class="{ 'text-orange-400': current}">{{
+	<a :class="{ 'text-orange-400': current}"
+	   :href="page + '.html'"
+	   class="no-underline rounded-md hover:bg-gray-800 hover:text-white px-3 md:px-6 py-2 md:py-3 block sm:inline text-center text-xl">{{
 			capPage
 		}}</a>
 </template>
