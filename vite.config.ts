16232ebe54622370d1c2f5d8a17f3907a5df0a0c
Donnie Adams <donnie@thedadams.com>
2024-08-01 20:24:23 -0400

    fix: ensure root path is home.html
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,36 +6,36 @@ import {resolve} from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	},
-	server: {
-		open: "/home.html",
-		port: 3000,
-	},
-	root: ".",
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "home.html"),
-				"home.html": resolve(__dirname, "home.html"),
-				"about.html": resolve(__dirname, "about.html"),
-				"basketball.html": resolve(__dirname, "basketball.html"),
-				"blog.html": resolve(__dirname, "blog.html"),
-				"contrastdots.html": resolve(__dirname, "contrastdots.html"),
-				"credits.html": resolve(__dirname, "credits.html"),
-				"links.html": resolve(__dirname, "links.html"),
-				"minimal.html": resolve(__dirname, "minimal.html"),
-				"pastprojects.html": resolve(__dirname, "pastprojects.html"),
-				"projects.html": resolve(__dirname, "projects.html"),
-				"research.html": resolve(__dirname, "research.html"),
-				"verticaldials.html": resolve(__dirname, "verticaldials.html"),
-				"watchfaces.html": resolve(__dirname, "watchfaces.html"),
-				"uses.html": resolve(__dirname, "uses.html")
-			},
-		},
-	},
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    server: {
+        open: "/home.html",
+        port: 3000,
+    },
+    root: ".",
+    build: {
+        rollupOptions: {
+            input: {
+                "/": resolve(__dirname, "home.html"),
+                "home.html": resolve(__dirname, "home.html"),
+                "about.html": resolve(__dirname, "about.html"),
+                "basketball.html": resolve(__dirname, "basketball.html"),
+                "blog.html": resolve(__dirname, "blog.html"),
+                "contrastdots.html": resolve(__dirname, "contrastdots.html"),
+                "credits.html": resolve(__dirname, "credits.html"),
+                "links.html": resolve(__dirname, "links.html"),
+                "minimal.html": resolve(__dirname, "minimal.html"),
+                "pastprojects.html": resolve(__dirname, "pastprojects.html"),
+                "projects.html": resolve(__dirname, "projects.html"),
+                "research.html": resolve(__dirname, "research.html"),
+                "verticaldials.html": resolve(__dirname, "verticaldials.html"),
+                "watchfaces.html": resolve(__dirname, "watchfaces.html"),
+                "uses.html": resolve(__dirname, "uses.html")
+            },
+        },
+    },
 })
