efb89ca6e203f81d2d0b28ef2d2d8516dfc28461
Donnie Adams <donnie@thedadams.com>
2025-01-26 10:00:45 -0500

    chore: update uses
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

--- src/Pages/Uses.vue
+++ src/Pages/Uses.vue
@@ -7,7 +7,7 @@ import Centered from "../components/Centered.vue"</script>
 		<Centered class="ml-8">
 			<p>A list of the things I use and find useful for my work and personal life. Inspired by <a
 				href="https://uses.tech" target="_blank" title="uses.tech">uses.tech</a>.<br><span
-				class="italic text-gray-400 text-sm">Last updated: October 6th, 2024</span>
+				class="italic text-gray-400 text-sm">Last updated: January 26th, 2025</span>
 			</p>
 			<h2 class="mt-6 text-blue-400">Hardware</h2>
 			<ul class="list-inside list-disc ml-6">
@@ -17,18 +17,21 @@ import Centered from "../components/Centered.vue"</script>
 				<li class="py-1.5"><span class="font-semibold">Work Computer</span>: MacBook Pro 16" (2022) with M1 Max,
 					32GB RAM, 1TB SSD
 				</li>
-				<li class="py-1.5"><span class="font-semibold">Secondary Work Computer</span>: Lenovo ThinkPad T14 Gen 5,
-					16GB RAM, 512GB SSD
+				<li class="py-1.5"><span class="font-semibold">Secondary Work Computer</span>: Lenovo ThinkPad T14 Gen
+					5, 16GB RAM, 512GB SSD
 				</li>
 				<li class="py-1.5"><span class="font-semibold">Personal Computer</span>: MacBook Pro 14" (2021) with M1
 					Pro, 32GB RAM, 1TB SSD
 				</li>
-				<li class="py-1.5"><span class="font-semibold">Phone</span>: iPhone 14 Pro with 128GB</li>
-				<li class="py-1.5"><span class="font-semibold">Watch</span>: Apple Watch Series 6, 44mm, Cellular</li>
+				<li class="py-1.5"><span class="font-semibold">Phone</span>: iPhone 16 Pro Max with 256GB</li>
+				<li class="py-1.5"><span class="font-semibold">Watch</span>: Apple Watch Series 10, Aluminum, Jet Black,
+					46mm, Cellular
+				</li>
 				<li class="py-1.5"><span class="font-semibold">Monitor</span>: Apple Studio Display</li>
 				<li class="py-1.5"><span class="font-semibold">Keyboard</span>: Keychron Q11 Split Design</li>
 				<li class="py-1.5"><span class="font-semibold">Mouse</span>: Logitech MX Master Wireless Mouse</li>
 				<li class="py-1.5"><span class="font-semibold">Headphones</span>: Sennheiser HD 660S2</li>
+				<li class="py-1.5"><span class="font-semibold">Earbuds</span>: Apple AirPods Pro 2</li>
 				<li class="py-1.5"><span class="font-semibold">Microphone</span>: Blue Yeti with pop filter</li>
 				<li class="py-1.5"><span class="font-semibold">Thunderbolt Hub</span>: SonnetTech Echo 20 Thunderbolt 4
 					SuperDock
@@ -39,29 +42,35 @@ import Centered from "../components/Centered.vue"</script>
 			<ul class="list-inside list-disc ml-6">
 				<li class="py-1.5"><span class="font-semibold">IDE</span>: JetBrains All-Product Pack, mostly GoLand
 				</li>
-				<li class="py-1.5"><span class="font-semibold">AI Coding Assistant</span>: Codeium</li>
+				<li class="py-1.5"><span class="font-semibold">AI Coding Assistant</span>: Jet Brains AI</li>
 				<li class="py-1.5"><span class="font-semibold">Terminal</span>: Apple Terminal</li>
-				<li class="py-1.5"><span class="font-semibold">Shell</span>: (oh-my-)zsh</li>
+				<li class="py-1.5"><span class="font-semibold">Shell</span>: Elvish</li>
 				<li class="py-1.5"><span class="font-semibold">Shell Prompt</span>: Starship</li>
 				<li class="py-1.5"><span class="font-semibold">Font</span>: SF Mono</li>
 				<li class="py-1.5"><span class="font-semibold">Local Containerization</span>: Rancher Desktop</li>
 			</ul>
 			<h2 class="mt-6 text-blue-400">Analog Tools</h2>
 			<ul class="list-inside list-disc ml-6">
-				<li class="py-1.5"><span class="font-semibold">Notebook</span>: Baron Fig Confidant, Flagship, Dot Grid</li>
-				<li class="py-1.5"><span class="font-semibold">Pen</span>: Stilform AEON Pure Pencil, Aluminum, Black</li>
-				<li class="py-1.5"><span class="font-semibold">Notebook Case</span>: Baron Fig Guardian Pro, Fig Wine</li>
+				<li class="py-1.5"><span class="font-semibold">Notebook</span>: Baron Fig Confidant, Flagship, Dot Grid
+				</li>
+				<li class="py-1.5"><span class="font-semibold">Pencil</span>: Stilform AEON Pure Pencil, Aluminum, Black
+				</li>
+				<li class="py-1.5"><span class="font-semibold">Pen</span>: Stilform FLOW Rollerball Pen, Aluminum,
+					Black
+				</li>
+				<li class="py-1.5"><span class="font-semibold">Notebook Case</span>: Baron Fig Guardian Pro, Fig Wine
+				</li>
 			</ul>
 			<h2 class="mt-6 text-blue-400">Software</h2>
 			<ul class="list-inside list-disc ml-6">
-				<li class="py-1.5"><span class="font-semibold">Launcher</span>: Raycast</li>
+				<li class="py-1.5"><span class="font-semibold">Launcher</span>: Spotlight</li>
 				<li class="py-1.5"><span class="font-semibold">Menu Bar Organizer</span>: Bartender</li>
 				<li class="py-1.5"><span class="font-semibold">System Monitor</span>: iStat Menus</li>
 				<li class="py-1.5"><span class="font-semibold">Podcast Player</span>: Pocket Casts</li>
 				<li class="py-1.5"><span class="font-semibold">Password Manager</span>: Apple Passwords</li>
 				<li class="py-1.5"><span class="font-semibold">Web Browser</span>: Safari</li>
 				<li class="py-1.5"><span class="font-semibold">Crypto Wallet</span>: Exodus</li>
-				<li class="py-1.5"><span class="font-semibold">RSS Reader</span>: Apple Reading List</li>
+				<li class="py-1.5"><span class="font-semibold">RSS Reader</span>: Apple Reading List, Tapestry</li>
 			</ul>
 			<h2 class="mt-6 text-blue-400">Services</h2>
 			<ul class="list-inside list-disc ml-6">
