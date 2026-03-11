a9764c77d337e90a6870db76f14e7eef09d7a05d
Donnie Adams <donnie@thedadams.com>
2026-03-11 15:58:50 -0400

    fix: update cloudflare IP updater script
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

--- assets/images/CFDNSUpdater.py
+++ assets/images/CFDNSUpdater.py
@@ -1,43 +1,67 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
+import json
 import urllib.request
 from urllib.parse import urlencode
-import json
 
 # These should be changed.
-cloudflareEmail = "EMAIL-ADDRESS"
-cloudflareAPIkey = "YOUR-API-KEY"
-baseDomain = "YOURDOMAIN.COM"
-subDomain = "SUBDOMAIN"
+cloudflare_api_key = "xMIkUbaXaW1FmnBtkS2FIbadnM-k5Rgg_K-AQYwA"
+base_domain = "thedadams.com"
+sub_domain = "office"
 
 # These should NOT be changed.
-recordType = "A"
-zoneID = ""
-dnsID = ""
+record_type = "A"
+zone_id = ""
+dns_id = ""
 
 # Get the zoneID from CloudFlare.
-headers = {'X-Auth-Key': cloudflareAPIkey, 'X-Auth-Email': cloudflareEmail, 'Content-Type': "application/json"}
-req = urllib.request.Request("https://api.cloudflare.com/client/v4/zones/", headers=headers)
+headers = {
+    "Authorization": "Bearer " + cloudflare_api_key,
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
+req = urllib.request.Request(
+    "https://api.cloudflare.com/client/v4/zones/", headers=headers
+)
 for item in json.loads(urllib.request.urlopen(req).read().decode("utf-8"))["result"]:
-    if item["name"] == baseDomain:
-        zoneID = item["id"]
+    if item["name"] == base_domain:
+        zone_id = item["id"]
         break
 
 # Find the IP address that CloudFlare has for your (sub)domain based on the recordName and recordType
-data = {"type": recordType, "name": subDomain + "." + baseDomain}
-req = urllib.request.Request("https://api.cloudflare.com/client/v4/zones/" + zoneID + "/dns_records?%s" % urlencode(data), headers=headers)
+data = {"type": record_type, "name": sub_domain + "." + base_domain}
+req = urllib.request.Request(
+    "https://api.cloudflare.com/client/v4/zones/"
+    + zone_id
+    + "/dns_records?%s" % urlencode(data),
+    headers=headers,
+)
 response = json.loads(urllib.request.urlopen(req).read().decode("utf-8"))["result"][0]
-IPFromCF = response["content"]
-dnsID = response ["id"]
+ip_from_cf = response["content"]
+dns_id = response["id"]
 
 # Get your current device IP Address
-response = json.loads(urllib.request.urlopen("http://ip-api.com/json").read().decode("utf-8"))
-ActualIP = response["query"]
+response = json.loads(
+    urllib.request.urlopen("http://ip-api.com/json").read().decode("utf-8")
+)
+actual_ip = response["query"]
 
-if IPFromCF != ActualIP:
+if ip_from_cf != actual_ip:
     # Update with Cloudflare
-    data = {"type": recordType, "name": subDomain + "." + baseDomain, "content": ActualIP}
-    req = urllib.request.Request("https://api.cloudflare.com/client/v4/zones/" + zoneID + "/dns_records/" + dnsID, headers=headers, data=json.dumps(data).encode("utf-8"),  method='PUT')
+    data = {
+        "type": record_type,
+        "name": sub_domain + "." + base_domain,
+        "content": actual_ip,
+    }
+    req = urllib.request.Request(
+        "https://api.cloudflare.com/client/v4/zones/"
+        + zone_id
+        + "/dns_records/"
+        + dns_id,
+        headers=headers,
+        data=json.dumps(data).encode("utf-8"),
+        method="PUT",
+    )
     success = json.loads(urllib.request.urlopen(req).read().decode("utf-8"))["success"]
     if success:
         # You can run some code on success.
